3c4b77de96a10091fe72bcc1407fd8f128094435
laurianie guiteau <laurianie_guiteau24@milton.edu>
2023-05-15 21:48:37 -0400

    5.15.23

--- src/WikiGame.java
+++ src/WikiGame.java
@@ -1,6 +1,4 @@
 
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
@@ -11,16 +9,18 @@ import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.HashSet;
+
 
 public class WikiGame implements ActionListener {
     private JFrame mainFrame;
     private JTextArea inputURL;
-    private JTextArea search;
+    private JTextArea inputEndURL;
     private JTextArea ta;
     private JLabel URL;
-    private JLabel SEARCH;
+    private JLabel EndURL;
     private JButton start;
+    private Font font;
+
 
     private JPanel panel;
     private JScrollPane scrollBar;
@@ -28,21 +28,21 @@ public class WikiGame implements ActionListener {
     private int WIDTH = 1400;
     private int HEIGHT = 1200;
 
-
-    private int maxDepth;
     private ArrayList<String> noDupes = new ArrayList<String>();
-
-    ArrayList<String> Path = new ArrayList<String>();
+    private ArrayList<String> Path = new ArrayList<String>();
 
     boolean foundlink = false;
     boolean noTwice = false;
     int counterr = 0;
+    int DEPTH = 2;
 
 
 
-    String startLink = "https://en.wikipedia.org/wiki/Zendaya";
-    String endLink = "https://en.wikipedia.org/wiki/Universal_Pictures";
-//            "https://en.wikipedia.org/wiki/Timoth%C3%A9e_Chalamet";
+//    TEST RUN 1 = https://en.wikipedia.org/wiki/Zendaya (DOES NOT WORK)
+//    TEST RUN 1 = https://en.wikipedia.org/wiki/Abbott_Elementary (DOES NOT WORK)
+//    TEST RUN 2 = https://en.wikipedia.org/wiki/Beyonc%C3%A9 (WORKS)
+//    TEST RUN 2 = https://en.wikipedia.org/wiki/The_Lion_King_(2019_film) (WORKS)
+
 
 
     public static void main(String[] args) {
@@ -50,64 +50,33 @@ public class WikiGame implements ActionListener {
         w.prepareGUI();
     }
 
-//    public void WikiGame() {
-//        // beginning link, where the program will start
-////        String endLink = "https://en.wikipedia.org/wiki/Timoth%C3%A9e_Chalamet";    // ending link, where the program is trying to get to
-////        String endLink = "https://en.wikipedia.org/wiki/K.C._Undercover";
-//        maxDepth = 2;           // start this at 1 or 2, and if you get it going fast, increase
-//
-////        if (depthFirstSearch(startLink, endLink, 2)) {
-////            System.out.println("found it !!");
-////            path.add(startLink);
-////        } else {
-////            System.out.println("did not find it !!");
-////        }
-//
-//    }
-
-    // recursion method, you'll probably want to rename it
-    // you may want this method to return something or take additional parameters
-
-    public boolean depthFirstSearch(String startLink, String endLink, int depth) {
-        // recursion method
-        //public boolean findLink(String startLink, String endLink, int depth) {
-
-        System.out.println("depth is: " + depth + ", link is: https://en.wikipedia.org" + startLink);
-
-        // BASE CASE
-        if (startLink.equals(endLink)) {
-            System.out.println(endLink);
-            System.out.println("found it !!");
-        } else if (depth == maxDepth) {
-            System.out.println("not found");
-        }
-        // GENERAL RECURSIVE CASE
-        else {
-           // WikiGame();
-            for (int i = 0; i > noDupes.size(); i++) {
-//               depthFirstSearch();
-            }
-
-        }
-        return false;
-    }
-
-
 
     private void prepareGUI() {
+        font = new Font("Normal", Font.ITALIC, 20);
+
         mainFrame = new JFrame("Wiki Game");
         mainFrame.setSize(WIDTH, HEIGHT);
         mainFrame.setLayout(new BorderLayout());
         panel = new JPanel();
-        panel.setLayout(new GridLayout(2,3));
+        panel.setLayout(new GridLayout(3,4));
         mainFrame.add(panel,BorderLayout.NORTH);
 
 
         inputURL = new JTextArea();
+        inputEndURL = new JTextArea();
 
         ta = new JTextArea("RESULTS: " + "\n");
+        ta.setBackground(Color.cyan);
+        ta.setForeground(Color.blue);
+        ta.setFont(font);
         URL = new JLabel ("INPUT URL: ");
         URL.setSize(2,2);
+        URL.setFont(font);
+        URL.setForeground(Color.blue);
+        EndURL = new JLabel ("INPUT END URL: ");
+        EndURL.setSize(2,2);
+        EndURL.setFont(font);
+        EndURL.setForeground(Color.blue);
 
         start = new JButton ("START ");
         start.setActionCommand("START");
@@ -117,6 +86,8 @@ public class WikiGame implements ActionListener {
         panel.add(start);
         panel.add(URL);
         panel.add(inputURL);
+        panel.add(EndURL);
+        panel.add(inputEndURL);
         panel.add(start);
         mainFrame.add(ta,BorderLayout.CENTER);
         scrollBar = new JScrollPane(ta);
@@ -127,21 +98,19 @@ public class WikiGame implements ActionListener {
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
-            }
-        });
+            }});
 
         mainFrame.setVisible(true);
     }
 
 
-    public void Wikigame( String originalLink, String fLink, int depth){
+    public void Wikigame(String originalLink, String fLink, int depth){
 
         Path.add(originalLink);
 
+
         if (foundlink == false) {
             try {
-                //          System.out.println();
-                //       URL url = new URL (inputURL.getText());
                 URL url = new URL(originalLink);
                 BufferedReader reader = new BufferedReader(
                         new InputStreamReader(url.openStream())
@@ -153,7 +122,6 @@ public class WikiGame implements ActionListener {
                             int start = line.indexOf("href=") + 6;
 
                             line = line.substring(start);
-                            //                    System.out.println(line);
                             int end = line.indexOf("\"");
                             int end2 = line.indexOf("\'");
 
@@ -196,11 +164,10 @@ public class WikiGame implements ActionListener {
                                         ta.append("This is the path: ");
 
                                         for (int i = 0; i < Path.size(); i++) {
-                                            System.out.println("This is the path: ");
                                             System.out.println(Path.get(i));
 
+                                            ta.append("\n ↓");
                                             ta.append(" \n ");
-                                            ta.append("↓");
                                             ta.append(Path.get(i));
                                             ta.append("\n");
                                         }
@@ -208,14 +175,13 @@ public class WikiGame implements ActionListener {
                                         Path.clear();
                                     }
 
-                                    if (depth <= 2 && foundlink == false) {
+                                    if (depth <= DEPTH && foundlink == false) {
                                         Wikigame(finalLink, fLink, depth + 1);
                                     }
                                 }
                             }
                             line = line.substring(end);
                         }
-
                     }
 
                 print();
@@ -224,24 +190,10 @@ public class WikiGame implements ActionListener {
                 System.out.println(ex);
             }
 
-
             if (foundlink == false && Path.size() >= 1) {
                 Path.remove(Path.size() - 1);
             }
-
-
-
-        }
-
-
-
-
-
-
-
-
-
-        }
+        }}
 
     public void print(){
         for(int i = 0; i > noDupes.size(); i++){
@@ -257,13 +209,16 @@ public class WikiGame implements ActionListener {
     private class ButtonClickListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
+
             if (command.equals("START")){
                 start.setText("START clicked ");
-                Wikigame(startLink,endLink, 2);
+                System.out.println("The depth is: " + DEPTH);
+                Wikigame(inputURL.getText(),inputEndURL.getText(), 2);
+                if (foundlink == false){
+                    System.out.println("This link could not be found :( ");
+                    ta.append("\n THIS LINK COULD NOT BE FOUND :( ");
+                }
             }
-        }
-
-        private void WikiGame() {
         }
     }
 }
